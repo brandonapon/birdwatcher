66bbda234593cdd3a5cac3c0c799b4b73247442c
Sydney Ma <siyi.ma0523@gmail.com>
2019-01-27 01:43:56 -0800

    script to transform dumped_data(old api) to csv file
    Signed-off-by: Sydney Ma <siyi.ma0523@gmail.com>

---
+++ archive/old_api/json_to_csv.py
@@ -0,0 +1,38 @@
+import csv
+import json
+import os
+import datetime
+
+INPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data_dump")
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "csv_output.csv")
+
+def parse_filename_to_timestamp(filename):
+    year, month, date, hour, minute, second = filename[0:4], filename[4:6], \
+                                               filename[6:8], filename[9:11], \
+                                               filename[11:13], filename[13:15]
+    
+    time_stamp = datetime.datetime(*[int(x) for x in [year, month, date, hour, minute, second]])
+    return int(time_stamp.timestamp())
+
+def main():
+    with open(OUTPUT_DIR, "w") as write_file:
+        csv_writer = csv.writer(write_file)
+        csv_writer.writerow(['id', 'latitude', 'longitude', 'battery_level', 'captive', 'time_stamp'])
+        for dump in os.listdir(INPUT_DIR):
+            if not dump.startswith('output'): 
+                with open(INPUT_DIR + "/" + dump, "r") as read_file:
+                    data = read_file.readlines()
+                    output = json.loads(data[1])
+
+                    for item in output['birds']:
+                        csv_writer.writerow([
+                            item['id'], 
+                            item['location']['latitude'], 
+                            item['location']['longitude'],
+                            item['battery_level'],
+                            item['captive'],
+                            parse_filename_to_timestamp(dump)
+                        ])
+                        
+if __name__ == "__main__":
+    main()
